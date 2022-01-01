23e061b3191740cd28fe0c8a483c3de3bc4cf9ca
3quarterscolon <94267030+3quarterscolon@users.noreply.github.com>
2022-01-01 05:20:30 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+from random import choice
+
+####
+d = 3 # do not change
+n = 500
+start = "long I stood"
+####
+
+with open("poetry.txt") as f:
+  poems = f.readlines()
+  for i, item in enumerate(poems): poems[i] = item.split()
+
+patterns = {}
+for poem in poems:
+  #print(poem)
+  for i, _ in enumerate(poem):
+    try:
+      key = ' '.join([poem[i + item] for item in range(d)])
+      if not key in patterns.keys():
+        patterns[key] = [poem[i + d]]
+      else:
+        patterns[key].append(poem[i + d])
+    except: break
+#print(patterns)
+
+generated = start.split()
+try:
+	for i in range(n):
+		generated.append(choice(patterns[" ".join(generated[-d:])])) # temporary solution while I figure out how to make this work for variable values of d
+except: pass
+print(" ".join(generated))
